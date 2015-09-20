c58f136030d3c3000fb16c6bb050c3c021a69f0d
eMobc SL <emobc@eMobcs-MacBook-Pro.local>
2015-09-20 01:57:44 -0700

    Get data

--- relief/js/app.js
+++ relief/js/app.js
@@ -17,8 +17,6 @@
 
       // on map load
       map.on("load", function () {
-
-          // grab all quakes
           var Quakes = Parse.Object.extend("quakes");
           var query = new Parse.Query(Quakes);
           query.descending("createdAt");
@@ -26,10 +24,6 @@
           query.limit(100);
           query.find({
             success: function(objects) {
-              
-             console.log(objects);
-
-             // loop thru all quakes
              $.each(objects, function(index, quake) {
                   var symbolPoint = new SimpleMarkerSymbol(SimpleMarkerSymbol.STYLE_CIRCLE, Math.pow(quake.get('mag'), 10) / 10000000,
                                     new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID, new Color([255, 0, 0]), 1),
@@ -44,10 +38,85 @@
               console.log("Error: " + error.code + " " + error.message);
             }
           });
-
-          
-
       });
 
-  });
+      function Donation(name, createdAt, location, amount) {
+        this.name = name;
+        this.createdAt = createdAt;
+        this.location = location;
+        this.amount = amount;
 
+        this.print = function() {
+          $('#donations').append('<h2>'+this.name+'</h2> \
+            <div class="row"> \
+              <div class="col-md-5"> \
+                <i class="fa fa-clock-o"></i> '+this.createdAt+' 4hrs ago</div> \
+              <div class="col-md-5 money"> \
+              <i class="fa fa-usd"></i> \
+              <span>$'+this.amount+' raised from '+this.location.latitude+','+this.location.longitude+'</span> \
+            </div> \
+          </div>');
+        }
+      }
+
+      function printData(donation) {
+          $.each(donation, function(index, donation) {
+            donation.print();
+      });
+        }
+      
+      function Donations() {
+        this.donation = [];
+
+        this.print = 
+        
+        this.getData = function() {
+          Parse.initialize("ahKpQvGXaUQHQ1iCyNGyccBU1hz6UsYIWu1HQcwg", "g15tPzTig1ocoqTPFAiuZvTtYb5iq8QlgOURaZkl");
+          var donationsParse = Parse.Object.extend("donations");
+          var donationsData = new Parse.Query(donationsParse);
+          donationsData.limit(10);
+
+          (function(donation){
+            donationsData.find({
+             success: function(donationCollection) {
+
+              $.each(donationCollection, function(index, donationData) {    
+                var quakes = Parse.Object.extend("quakes");
+                var quakesData = new Parse.Query(quakes);
+                (function(donation){
+                  console.log(donationData.get('quake'));
+                  quakesData.get(donationData.get('quake'), {       
+                    success: function(object) {
+                      donationElement = new Donation(object.get('place'), donationData.createdAt, donationData.get('location'), donationData.get('amount'));
+                      donation.push(donationElement); 
+
+                      if (donation.length == 2) {
+                      printData(donation);
+                    }
+                    
+                      
+                    },
+                    error: function(object, error) {
+                    }
+
+                  }); 
+
+                })(donation);         
+             });
+            },
+            error: function(error) {
+              console.log("Error: " + error.code + " " + error.message);
+            }
+          });
+        })(this.donation);
+      }
+    }
+      
+      var donations = new Donations();
+      donations.getData();
+      console.log("FINAL END");
+      console.log(donations.donation);
+   
+      $.each(donations.donation, function(index, donation) {
+      });
+  });
